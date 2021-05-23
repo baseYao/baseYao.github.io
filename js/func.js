718840fa11822dc3a2a933ca219ed1d562725673
yao <logicfire@163.com>
2021-05-23 11:31:49 +0800

    second planc

--- js/func.js
+++ js/func.js
@@ -1,19 +1,35 @@
-function getDate(){
-    var mydate = new Date();
-    var years = mydate.getFullYear();//年
-    var month = mydate.getMonth();
-    var day = mydate.getDate();
-    var days = mydate.getDay();
-    switch(days){
-        case 1: days = '星期一'; break;
-        case 2: days = '星期二'; break;
-        case 3: days = '星期三'; break;
-        case 4: days = '星期四'; break;
-        case 5: days = '星期五'; break;
-        case 6: days = '星期六'; break;
-        case 7: days = '星期日'; break;
-        default: days = "系统错误!"
+var txt = document.getElementById("text"); 
+var oUl = document.getElementById("list"); 
+txt.onkeyup = function(){ 
+    var val = txt.value; 
+    if(yinqin){
+        var oScript = document.createElement("script");//动态创建script标签 
+        oScript.src = "https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su?wd="+val+"&cb=callback"; 
+        //添加链接及回调函数 
+        document.body.appendChild(oScript);//添加script标签 
+        document.body.removeChild(oScript);//删除script标签 
     }
-    var strs = year + "年" + month + "月" + day + "日" + days;
-    return strs;
+    /*input框中按下回车根据input的值跳转页面*/
+    if(event.keyCode==13){
+        /*将百度作为连接,传入input的值,并跳入新的页面*/
+        cliksearch();
+    }
+}
+document.body.onclick = function(){
+    oUl.style.display = "none";
+}
+//回调函数 
+function callback(data){ 
+    var str=""; 
+    for(var i=0;i<data.s.length;i++){ 
+    str += "<li><a target=\"_blank\"  href=\"https://www.baidu.com/s?wd="+data.s[i]+"\">"+data.s[i]+"</a></li>"; 
+    } 
+    //console.log(str); 
+    oUl.innerHTML=str; 
+    oUl.style.display="block"; 
+}
+function cliksearch(){
+    // var url = "https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd="+txt.value
+    url += txt.value
+    window.open(url, '_blank').location;
 }
